6963caf22864e19f8483a5388523092d61b17642
Juan Josè Gil <matero@gmail.com>
2018-05-03 12:06:58 -0300

    initial definition

---
+++ grails-app/init/gorm/neo4j/cant/report/errors/Application.groovy
@@ -0,0 +1,10 @@
+package gorm.neo4j.cant.report.errors
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
---
+++ grails-app/init/gorm/neo4j/cant/report/errors/BootStrap.groovy
@@ -0,0 +1,9 @@
+package gorm.neo4j.cant.report.errors
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
